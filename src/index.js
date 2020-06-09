aa28c8eb4c6340ce8d9adad3c90b1e4d993ff358
石田航也 <k.157.2aic@gmail.com>
2020-06-09 16:19:39 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import _ from "lodash"
 
 function component (){
   const element = document.createElement("div");
-  const array = ["hello", "webpack"]
+  const array = ["hello", "webpack","!!"]
   element.innerHTML = _.join(array,"")
   return element
 }
+
 document.body.appendChild(component())
