0bbcdb5ea37fa4883432b450270c6717090471eb
石田航也 <k.157.2aic@gmail.com>
2020-06-09 18:31:22 +0900

    inplement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import _ from "lodash"
+// import {Nijou,NAME} from "./utilitys"
+// import * as  utilitys from "./utilitys"
+//名前の競合を避ける
+//import {NAME as NAME_OF_HAM } from "./utilitys"
+
+//defaultの場合はLionで無くても名前はなんでもOK
+import Lion from "./utilitys"
+
+// console.log(Nijou(10))
+// console.log(NAME)
+// console.log(utilitys.Nijou(10))
+// console.log(utilitys.NAME)
+//console.log(NAME_OF_HAM)
+
+console.log(Lion.say())
 
 function component (){
   const element = document.createElement("div");
